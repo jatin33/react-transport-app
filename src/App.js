12226bc6da82f010774880e50fb2f85f2dd5d6a2
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-05-26 21:50:25 +0530

    Overall App Components created

--- src/App.js
+++ src/App.js
@@ -2,23 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class TransportTitle extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test title</h1>
+        <h2>Test description</h2>
+      </div>
+    )
+  }
+}
+
+class TransportOption extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <label for="newonly">
+          New Only
+          <input type="checkbox" name="newonly" />
+        </label>
+        <label for="newonly">
+          Select Type
+          <select>
+            <option value="convertible">Convertible</option>
+            <option value="car">Car</option>
+            <option value="truck">Truck</option>
+          </select>
+        </label>
+      </div>
+    )
+  }
+}
+
+class VehicleDetails extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TransportTitle />
+      <TransportOption />
+      
     </div>
   );
 }
